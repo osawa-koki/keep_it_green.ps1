e34de6c9915774815331d46a159e6baff7b2046a
osawa-koki <github@osawa.dev>
2022-11-11 15:02:10 +0000

    実行確認まで。

--- app/main.ps1
+++ app/main.ps1
@@ -24,7 +24,44 @@ while ($true) {
   # 開始日時を取得
   $start_datetime = $current_datetime.AddDays(-$days_before)
 
-  Write-Host $start_datetime
+  # 一日に最低何回コミットするか取得
+  while ($true) {
+    $commit_count_min = Read-Host "一日に最低何回コミットしますか??? -> ".Trim()
+
+    if ($commit_count_min -match "^[0-9]+$") {
+      break
+    }
+    Clear-Host
+    Write-Host "適当な数値を入力してください。"
+  }
+
+  # 一日に最高何回コミットするか取得
+  while ($true) {
+    $commit_count_max = Read-Host "一日に最高何回コミットしますか??? -> ".Trim()
+
+    if ($commit_count_max -match "^[0-9]+$") {
+      if ($commit_count_min -le $commit_count_max) {
+        break
+      } else {
+        Clear-Host
+        Write-Host "最高値は最小値以上にしてください。"
+        continue
+      }
+    }
+    Clear-Host
+    Write-Host "適当な数値を入力してください。"
+  }
+
+  # 実行確認
+  Write-Host "以下の内容で実行します。よろしいですか？\n"
+  Write-Host "開始日時: $start_datetime\n"
+  Write-Host "一日に最低何回コミットしますか: $commit_count_min\n"
+  Write-Host "一日に最高何回コミットしますか: $commit_count_max\n"
+  $answer = Read-Host "実行しますか？(y/n) -> ".Trim()
+
+  if ($answer -ne "y") {
+    continue
+  }
 
 
 
